713e4e2ea9a9fe47fd4e83e22552d7f3bdf77852
Albert Slepak <41020147+FlareCoding@users.noreply.github.com>
2018-10-05 11:52:38 -0400

    Update IUITextElement.h

--- Includes/IUITextElement.h
+++ Includes/IUITextElement.h
@@ -23,7 +23,7 @@ public:
 	virtual int GetFontSize() { return this->FontSize; }
 	
 	// Returns text allignment
-	virtual int GetTextAllignment() { return this-TextAllignment; }
+	virtual int GetTextAllignment() { return this->TextAllignment; }
 
 	// Get the style of the font
 	virtual DWRITE_FONT_STYLE GetFontStyle() { return this->fontStyle; }
